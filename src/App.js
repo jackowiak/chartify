c452847dbf9bf8c4e0b83ddc9b439dae1cee801c
jackowiak <matyskiewicz.j@gmail.com>
2018-10-15 20:55:10 +0200

    version with fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button} from 'reactstrap';
+const queryString = require('query-string');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const parsed = queryString.parse(window.location.search);
+    const accessToken = parsed.access_token;
+
+    fetch("https://api.spotify.com/v1/me", {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(resp => resp.json()).then(data => console.log(data));
+
+    fetch("https://api.spotify.com/v1/me/playlists", {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(resp => resp.json()).then(data => console.log(data));
+  }
+
+  logSpotify = () => {
+    window.location = "http://localhost:8888/login";
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       Chartify
+       <Button onClick={this.logSpotify}>Log in to Spotify!</Button>
       </div>
     );
   }
