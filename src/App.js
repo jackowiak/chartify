9f63c3238c4cd846655d96e2bf9cde043a9b6823
jackowiak <matyskiewicz.j@gmail.com>
2018-10-17 21:50:13 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import React, { Component } from 'react';
 import {Button} from 'reactstrap';
+import axios from 'axios';
+import {TrackView} from './TrackView';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 const queryString = require('query-string');
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      options: {
+        type: ['artists', 'tracks'],
+        range: ['shortterm', 'mediumterm', 'longterm']
+      },
+      artists: {
+        shortterm: [],
+        mediumterm: [],
+        longterm: []
+      },
+      tracks: {
+        shortterm: [],
+        mediumterm: [],
+        longterm: []
+      },
+      currentType: null
+    }
   }
 
   componentDidMount() {
     const parsed = queryString.parse(window.location.search);
     const accessToken = parsed.access_token;
 
-    fetch("https://api.spotify.com/v1/me", {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(resp => resp.json()).then(data => console.log(data));
+    axios.get("https://api.spotify.com/v1/me/top/artists", {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    }).then(data => {
+      this.setState({
+        artists: {
+          ...this.state.artists,
+          shortterm: data
+        }
+      });
+    })
   }
 
   logSpotify = () => {
     window.location = "http://localhost:8888/login";
   }
 
+  handleType = (e) => {
+    this.setState({currentType: e.target.textContent})
+  }
+
   render() {
     return (
       <div>
-       Chartify
-       <Button onClick={this.logSpotify}>Log in to Spotify!</Button>
+        <Button onClick={this.logSpotify}>Log in to Spotify!</Button>
+        <ul>
+          {this.state.options.type.map((option, i) => {
+            return (
+              <li onClick={(e) => this.handleType(e)} key={i}>{option}</li>
+            )
+          })}
+        </ul>
+        {
+          this.state.currentType ?
+          <TrackView type={(this.state.currentType === "artists") ? this.state.artists : this.state.tracks} />
+          :
+          null
+        }
       </div>
     );
   }
---
+++ src/TrackView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TrackView = (props) => {
+  return (
+    <div>
+      {console.log(props.type)}
+    </div>
+  )
+}
